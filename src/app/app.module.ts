fe0ff658d9a1a48fadf1885574cff1b8b8dbcd51
Asger Arnbjørn Thyregod <asger.thyregod@gmail.com>
2022-05-13 11:31:14 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { NgModule } from '@angular/core';
+import { APP_BASE_HREF } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { NavbarService } from './Services/NavbarService';
+import { FooterComponent } from './Components/footer/footer.component';
+import { AboutComponent } from './Components/sections/about/about.component';
+import { SkillsComponent } from './Components/sections/skills/skills.component';
+import { ContactComponent } from './Components/sections/contact/contact.component';
+import { WelcomeComponent } from './Components/sections/welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    AboutComponent,
+    SkillsComponent,
+    ContactComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{provide: APP_BASE_HREF, useValue: '/'}, NavbarService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
